3b15a2b3947215d0e2722f25c9feb79c8f5949a0
JHeok <skdhd999@gmail.com>
2024-06-11 14:59:56 +0900

    DB sqlans

---
+++ test1.sql
@@ -0,0 +1,29 @@
+use test1;
+
+show tables;
+
+create table user(
+	id int(11) not null auto_increment primary key,
+    name varchar(50) not null unique,
+    password varchar(50) not null,
+    nickname varchar(50) not null
+    );
+
+commit;
+select * from user;
+
+select * from user where name = 'bbb' and password = 'bbb';
+
+create table board(
+	id int(11) not null auto_increment primary key,
+    title varchar(255) not null ,
+    content text not null,
+    created datetime not null,
+    create_user_id int(11) not null
+);
+
+select * from board;
+
+select * from board where id = 1;
+
+select * from board b left join user u on b.create_user_id = u.id where b.id = 7;
