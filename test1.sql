2564357b35bf5cb85c0542cc2b380bb3c8e6f1cf
JHeok <skdhd999@gmail.com>
2024-06-11 18:33:15 +0900

    DB sql문

--- test1.sql
+++ test1.sql
@@ -33,3 +33,23 @@ select * from board b left join user u on b.create_user_id = u.id where b.id = 7
 select b.*, u.name from board b left join user u on b.create_user_id = u.id;
 
 select b.*, u.name from board b left join user u on b.create_user_id = u.id limit 5, 5;
+
+create table reply(
+	id int(11) not null auto_increment primary key,
+    reply_content text not null,
+    created datetime not null,
+    board_id int(11) not null,
+    user_id int(11) not null
+);
+
+select * from reply;
+
+delete from reply where id = 2;
+
+select * 
+  from reply r left join board b 
+    on r.board_id = b.id left join user u 
+    on r.user_id = u.id 
+ where b.id = 2;
+
+select r.*, u.nickname from reply r left join board b on r.board_id = b.id left join user u on b.create_user_id = u.id where b.id = 2;
