519cbe7a319227e357f331815cc2d48b9921037c
JHeok <skdhd999@gmail.com>
2024-06-13 16:23:57 +0900

    관리자페이지 헤더

---
+++ admin/include/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ko">
+<head>
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- Basic Style -->
+<link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+    
+<!-- Basic Js -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js" charset="utf-8"></script>
+</head>
+<body>
+    <div>
+        <h2><a href="dashboard.php">dashboard</a> <a href="log_detail.php">log_detail</a></h2>
+    </div>
