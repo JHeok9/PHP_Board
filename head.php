bd3ed2180ea734ad7453d4669f8af73b05b65bdf
JHeok <skdhd999@gmail.com>
2024-06-13 10:10:32 +0900

    bootstrap 적용

--- head.php
+++ head.php
@@ -6,5 +6,10 @@ session_start();
     <head>
         <meta charset="utf-8">
         <title>board</title>
+        
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/main.css">
     </head>
-    <body>
+    <body class="text-center">
